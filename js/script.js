830814ef6e4c3782329bce90a153f93e60ef3b46
Pedro Leite <contato.pedro77@gmail.com>
2022-11-29 18:22:47 -0300

    feature: encrypt implemented

--- js/script.js
+++ js/script.js
@@ -1,20 +1,42 @@
-let aside = document.getElementById("aside-paragraph")
+let asideParagraph = document.getElementById("aside-paragraph")
 let button = document.createElement("button")
 let buttonString = document.createTextNode("Copiar")
 
 function changeAsideStyle() {
-  document.getElementById("girl-image").style.display = "none";
-  document.getElementById("aside-title").style.display = "none";
+  document.getElementById("girl-image").style.display = "none"
+  document.getElementById("aside-title").style.display = "none"
   
-  aside.classList.add("encrypted-text");
+  asideParagraph.classList.add("encrypted-text");
   button.appendChild(buttonString)
   button.classList.add("decrypt")
   button.classList.add("copy-button")
   aside.appendChild(button)
 }
 
-let text = document.querySelector("textarea").value
+function encryptText(text) {
+  let encryptedText = ""
+  for(let i = 0; i < text.length; i++) {
+    if(text[i] == "a") {
+      encryptedText = encryptedText + "ai"
+    } else if(text[i] == "e") {
+      encryptedText = encryptedText + "enter"
+    } else if(text[i] == "i"){
+      encryptedText = encryptedText + "imes"
+    } else if(text[i] == "o"){
+      encryptedText = encryptedText + "ober"
+    } else if(text[i] == "u"){
+      encryptedText = encryptedText + "ufat"
+    } else {
+      encryptedText = encryptedText + text[i]
+    }
+  }
 
-function encrypt(text) {
-  
+  return encryptedText
+}
+
+function encryptButton() {
+  let text = document.querySelector("textarea").value
+  changeAsideStyle()
+  let encryptedText = encryptText(text)
+  asideParagraph.innerHTML = encryptedText
 }
